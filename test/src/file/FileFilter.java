054327059c1b5e82acdcaa375444cd1f09060959
jiahao.fang <jiahao.fang@xiaobao100.com>
2019-01-07 20:05:24 +0800

    代码修改

--- test/src/file/FileFilter.java
+++ test/src/file/FileFilter.java
@@ -7,46 +7,44 @@ import java.util.regex.Pattern;
 
 public class FileFilter {
     //用来存放过滤后的文件
-    public static ArrayList<File> resultFiles = new ArrayList<>();
-    public static FileInputStream stream = null;
-    public static StringBuilder content = new StringBuilder();
-    public static final String rex = "(?<=\")(\\s*select|\\s*join).*?(?=\")";
-    public static final String sqlJoin = "join";
-    public static final String sqlOrg = "(=\\s*)[a-z]*(.OrgId)";
-    public static Pattern p = Pattern.compile(rex);
-    public static Matcher m;
+    private static ArrayList<File> resultFiles = new ArrayList<>();
+    private static FileInputStream stream = null;
+    private static StringBuilder content = new StringBuilder();
+    private static final String rex = "(?<=\")(\\s*select|\\s*join).*?(?=\")";
+    private static final String sqlJoin = "join";
+    private static final String sqlOrg = "(=\\s*)[a-z]*(.OrgId)";
+    private static Pattern p = Pattern.compile(rex);
+    private static Matcher m;
     //实现方法
     public static File[] Filter(File[] fileList){
-        for (int i =0; i < fileList.length;i++){
+        for (File file : fileList) {
             String line;
             //join语句条数
             int joinAllCount = 0;
             //OrgId条数
             int orgAllCount = 0;
-            File file = fileList[i];
             long fileLength = file.length();
             byte[] fileContent = new byte[Integer.parseInt(String.valueOf(fileLength))];
-            try{
-                if (file.length() != 0){
+            try {
+                if (file.length() != 0) {
                     stream = new FileInputStream(file);
                     stream.read(fileContent);
-                    line = new String(fileContent,"UTF-8");
+                    line = new String(fileContent, "UTF-8");
                     //消除所有换行和回车
-                    line = line.replaceAll("\r|\n","");
+                    line = line.replaceAll("\r|\n", "");
                     m = p.matcher(line);
-                    while (m.find()){
+                    while (m.find()) {
                         content.append(m.group());
                     }
                     //判断join语句中是否缺少OrgId
-                    joinAllCount = CalculationCount(content.toString(),sqlJoin);
-                    if (joinAllCount ==0)
+                    joinAllCount = CalculationCount(content.toString(), sqlJoin);
+                    if (joinAllCount == 0)
                         continue;
-                    orgAllCount = CalculationCount(content.toString(),sqlOrg);
-                    if (joinAllCount != orgAllCount){
-                        content.delete(0,content.length());
+                    orgAllCount = CalculationCount(content.toString(), sqlOrg);
+                    if (joinAllCount != orgAllCount) {
+                        content.delete(0, content.length());
                         resultFiles.add(file);
-                    }
-                    else{
+                    } else {
                         stream.close();
                         continue;
                     }
